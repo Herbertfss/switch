c92b45cc448e7b93b09890eea2d3a0129e0d78ac
herbertfss <herbertsantows@gmail.com>
2023-07-06 22:03:31 -0300

    exercicio de switch

---
+++ switch.js
@@ -0,0 +1,78 @@
+const mes = 1;
+switch(mes){
+    case 1:
+    console.log("Janeiro");
+    break;
+    case 2:
+    console.log("Fevereiro");
+    break;
+    case 3:
+    console.log("Março");
+    break;
+    case 4:
+    console.log("Abriu");
+    break;
+    case 5:
+    console.log("Maio");
+    break;
+    case 6:
+    console.log("Junho");
+    break;
+    case 7:
+    console.log("Julho");
+    break;
+    case 8:
+    console.log("Agosto");
+    break;
+    case 9:
+    console.log("Setembro");
+    break;
+    case 10:
+    console.log("Outubro");
+    break;
+    case 11:
+    console.log("Novembro");
+    break;
+    case 12:
+    console.log("Dezembro");
+    break;
+    default:
+        console.log("Não existe")
+    break;
+}
+
+// const letra = "5";
+// switch(letra){
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//     console.log("Vogal");
+//     break;
+//     case "b":
+//     case "c":
+//     case "d":
+//     case "f":
+//     case "g":
+//     case "h":
+//     case "j":
+//     case "k":
+//     case "l":
+//     case "m":
+//     case "n":
+//     case "p":
+//     case "q":
+//     case "r":
+//     case "s":
+//     case "v":
+//     case "w":
+//     case "x":
+//     case "y":
+//     case "z":
+//     console.log("consoante")
+//     break;
+//     default:
+//     console.log("default")
+//     break;
+// }
